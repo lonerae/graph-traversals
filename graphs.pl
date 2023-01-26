dd2b45b88df2c330077d91e0cc9b2f69cd5615ad
lonerae <78692050+lonerae@users.noreply.github.com>
2023-01-26 21:30:16 +0200

    updated DFS, first attempt at BFS

--- graphs.pl
+++ graphs.pl
@@ -9,28 +9,103 @@
 
 /*NON-WEIGHTED NON-DIRECTIONAL ACYCLIC*/
 
-connects(1,2).
-connects(1,4).
-connects(1,5).
-connects(2,7).
-connects(2,6).
-connects(2,3).
+connects(1,4). 
+connects(4,1).
+connects(1,5). 
+connects(5,1).
+connects(1,2). 
+connects(2,1).
+connects(2,7). 
+connects(7,2).
+connects(2,6). 
+connects(6,2).
+connects(2,3). 
+connects(3,2).
 
-edge(X,Y) :- connects(X,Y) ; connects(Y,X).
- 
-/*----------------------BFS--------------------------*/
+next_node(CurrentNode,Visited,NextNode) :-
+	connects(CurrentNode,NextNode),
+	not(member(NextNode,Visited)).
+
+adj(N,Res) :- findall(X,connects(N,X),Res).
+adj([N|Ns],Res) :- adj(N,PRes), adj(Ns,RRes), append([PRes],RRes,Res).
+
+/*----------------------DFS--------------------------*/
 /* Given:                                            */
-/*	starting node, target node, list with the    */
-/* 	starting node as its only element            */
+/*	starting node, target node                   */
 /* Returns:                                          */
-/* 	a path from the starting to the target       */
+/* 	all paths from the starting to the target    */
 /* 	node                                         */
 /*---------------------------------------------------*/
 
-dfs(FinishNode,FinishNode,_,[FinishNode]).
-dfs(StartNode,FinishNode,Visited,[StartNode|RestPath]) :-
-	edge(StartNode,X),
-	not(member(X,Visited)),
-	dfs(X,FinishNode,[X|Visited],RestPath).	
+dfs(Start,Target,Path) :- dfs(Start,Target,[Start],Path).
 
-/*---------------------------------------------------*/
+dfs(Target,Target,_,[Target]).
+dfs(Start,Target,Visited,[Start|RestPath]) :-
+	next_node(Start,Visited,Next),
+	dfs(Next,Target,[Next|Visited],RestPath).
+
+/*----------------------BFS--------------------------*/
+
+bfs(Start,Target,Path) :- bfs(Start,Target,[Start],Path).
+
+bfs(Target,Target,_,[Target]). 
+bfs(Start,Target,Visited,[Start|RestPath]) :-
+	adj(Start,[F|Res]),
+	not(member(F,Visited)),
+	bfs(F,Target,[F|Visited],RestPath).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
