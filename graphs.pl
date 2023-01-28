927d6e94a43f6c84a17db99f7101886be046bdd0
lonerae <78692050+lonerae@users.noreply.github.com>
2023-01-28 18:18:17 +0200

    functional bfs

--- graphs.pl
+++ graphs.pl
@@ -9,26 +9,66 @@
 
 /*NON-WEIGHTED NON-DIRECTIONAL ACYCLIC*/
 
-connects(1,4). 
+
 connects(4,1).
 connects(1,5). 
+connects(1,4). 
 connects(5,1).
 connects(1,2). 
-connects(2,1).
 connects(2,7). 
 connects(7,2).
 connects(2,6). 
 connects(6,2).
 connects(2,3). 
 connects(3,2).
+connects(2,1).
+connects(4,8).
+connects(8,4).
+connects(8,9).
+connects(9,8).
+
+/*----------------------COMMON FUNCTIONS-------------*/
 
 next_node(CurrentNode,Visited,NextNode) :-
 	connects(CurrentNode,NextNode),
 	not(member(NextNode,Visited)).
 
-adj(N,Res) :- findall(X,connects(N,X),Res).
-adj([N|Ns],Res) :- adj(N,PRes), adj(Ns,RRes), append([PRes],RRes,Res).
+adj([N|Ns],Visited,Res) :- 
+	adj(N,Visited,PRes), 
+	adj(Ns,Visited,RRes), 
+	append(PRes,RRes,Res), !.
 
+adj(N,Visited,Res) :- 
+	findall(X,(connects(N,X),not(member(X,Visited))),Res).
+/*adj([N|Ns],Visited,Res) :- 
+	findall(X,(connects(N,X),not(member(X,Visited))),Res).*/
+
+
+create_branches(_,[],[]).
+
+create_branches(Branch,[X|Xs],Branches) :- 
+	append([X],Branch,PBranch),
+	create_branches(Branch,Xs,RBranches),
+	append([PBranch],RBranches,Branches),!.
+
+/*check([],_,_) :- false.*/
+check([Branch|RBranches],Target,Path) :-
+	(
+		is_list(Branch),
+		(	
+			(member(Target,Branch),
+			append([],[Branch],Path));
+			check(RBranches,Target,Path)
+		)
+	);
+	(	
+		member(Target,[Branch|RBranches]),
+		append([],[Branch|RBranches],Path)
+	).
+
+replace(AllBranches,Branches,NewAllBranches) :-
+	append(AllBranches,Branches,NewAllBranches).
+	
 /*----------------------DFS--------------------------*/
 /* Given:                                            */
 /*	starting node, target node                   */
@@ -40,24 +80,35 @@ adj([N|Ns],Res) :- adj(N,PRes), adj(Ns,RRes), append([PRes],RRes,Res).
 dfs(Start,Target,Path) :- dfs(Start,Target,[Start],Path).
 
 dfs(Target,Target,_,[Target]).
+
 dfs(Start,Target,Visited,[Start|RestPath]) :-
 	next_node(Start,Visited,Next),
 	dfs(Next,Target,[Next|Visited],RestPath).
 
 /*----------------------BFS--------------------------*/
 
-bfs(Start,Target,Path) :- bfs(Start,Target,[Start],Path).
+bfs(Start,Target,Path) :- bfs([[Start]],Target,[Start],[Start],Path).
 
-bfs(Target,Target,_,[Target]). 
-bfs(Start,Target,Visited,[Start|RestPath]) :-
-	adj(Start,[F|Res]),
-	not(member(F,Visited)),
-	bfs(F,Target,[F|Visited],RestPath).
-
-
-
-
-
+bfs([Branch|RestBranches],Target,Visited,AllBranches,Path) :-
+	check(Branch,Target,Path);
+	branch_eval([Branch|RestBranches],Target,Visited,[Branch|RestBranches],Path).
+		
+branch_eval([Branch|RestBranches],Target,Visited,AllBranches,Path) :-
+	adj(Branch,Visited,Res),
+	create_branches(Branch,Res,Branches),
+	(
+		check(Branches,Target,Path);
+		append(Res,Visited,NewVisited),
+		append(AllBranches,Branches,NewAllBranches),
+		(
+			(
+				RestBranches \= [],
+				branch_eval(RestBranches,Target,NewVisited,NewAllBranches,Path)
+			);
+			bfs(NewAllBranches,Target,NewVisited,NewAllBranches,Path)
+		)
+	).
+		
 
 
 
